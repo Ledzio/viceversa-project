3e031bb16e0714b70f54c0f2df8ad686b7beac04
Ledzio <osalamaha1@gmail.com>
2022-07-07 02:01:07 +0300

    Added reversed text and words number, fixed textarea form

--- viceversa/views.py
+++ viceversa/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+
 def home(request):
 	return render (request, 'home.html')
+
+def reverse(request):
+	original_text = request.GET['usertext']
+	reversed_text1=original_text[::-1]
+	nubmer_of_words = len(original_text.split())
+	return render (request, 'reverse.html',{'straight_text':original_text, 'reversed_text':reversed_text1, 'words_number':nubmer_of_words}) 
